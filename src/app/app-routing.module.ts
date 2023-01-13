4cdb06f0bdb2d971d5476fa380491aa2c1ca5543
MohamedWaelGD <mohamedwael.gd@gmail.com>
2023-01-13 15:48:02 +0200

    Home Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientModule } from './components/pages/client/client.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./components/pages/client/client.module').then(m => m.ClientModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CustomModule } from './components/customModule/custom.module';
+import { CommonModule } from '@angular/common';
+import { ClientModule } from './components/pages/client/client.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    CustomModule,
+    ClientModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
